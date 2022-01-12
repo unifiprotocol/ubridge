9f7f05f08b4e1e5520e1c8ae96517476fbf9d281
Carlos Lostao <hastro1990@gmail.com>
2022-01-12 12:03:21 +0100

    test

--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,43 @@
-import { expect } from "chai";
-import { ethers } from "hardhat";
+import { expect } from 'chai'
+import { ethers } from 'hardhat'
+import { UBridge } from '../typechain'
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe('uBridge', function () {
+  let contractInstance: UBridge
+  beforeEach(async () => {
+    const [signer] = await ethers.getSigners()
+    const signerAddress = await signer.getAddress()
+    contractInstance = await deployContract(signerAddress, 1)
+  })
+  it('Should deploy contract and have the passed parameters', async function () {
+    const [signer] = await ethers.getSigners()
+    const signerAddress = await signer.getAddress()
+    expect(await contractInstance.verifyAddress(), 'Wrong verify address').to.be.equal(
+      signerAddress
+    )
+    expect(await (await contractInstance.chainId()).toNumber(), 'Wrong chain id').equal(1)
+  })
+  it('Should change verify address and return new one', async function () {
+    const [signer, second] = await ethers.getSigners()
+    const secondAddress = await second.getAddress()
+    await contractInstance.changeVerifySigner(secondAddress)
+    const verifyAddress = await contractInstance.verifyAddress()
+    expect(verifyAddress).equal(secondAddress)
+  })
+  it('Should add chainId=3 to supportedChainIds', async function () {
+    const [signer, second] = await ethers.getSigners()
+    const secondAddress = await second.getAddress()
+    const chainIdsToAdd = [2, 3]
+    await contractInstance.addChainId(chainIdsToAdd)
+    const [c1, c2] = await Promise.all(
+      chainIdsToAdd.map((chainId) => contractInstance.chainIdSupported(chainId))
+    )
+    expect(c1).true
+    expect(c2).true
+  })
+})
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
-});
+async function deployContract(verifyAddress: string, chainId: number) {
+  const UBridgeFactory = await ethers.getContractFactory('UBridge')
+  return UBridgeFactory.deploy(verifyAddress, chainId)
+}
