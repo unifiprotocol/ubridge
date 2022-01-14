79fea36ef04105598f0bdb71343538dbfcc25ea6
Carlos Lostao <hastro1990@gmail.com>
2022-01-14 11:00:39 +0100

    feat: added deploy script to show how to init bridge contract as if it were a constructor

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,28 +3,27 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+import { ethers } from "hardhat"
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
-
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  const [signer] = await ethers.getSigners()
+  const VERIFIER_ADDRESS = await signer.getAddress()
+  const CHAIN_ID = 1
+  const UBridgeFactory = await ethers.getContractFactory("UBridge")
+  const bridgeInstance = await UBridgeFactory.deploy()
+  const BridgeProxyFactory = await ethers.getContractFactory("ProxyBridge")
+  const initFunction = UBridgeFactory.interface.getFunction("init")
+  const initCallData = UBridgeFactory.interface.encodeFunctionData(initFunction, [
+    VERIFIER_ADDRESS,
+    CHAIN_ID
+  ])
+  const bridgeProxyInstance = await BridgeProxyFactory.deploy(bridgeInstance.address, initCallData)
+  console.log(`Contract successfully deployed: ${bridgeProxyInstance.address}`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  console.error(error)
+  process.exitCode = 1
+})
