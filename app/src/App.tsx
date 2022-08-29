32a4aea46ac280ea60caa35ff7410e90f00c361e
Daniel Blanco Parla <dblancoparla@gmail.com>
2022-08-29 23:30:47 +0200

    feat: ModuleFederationPlugin changes

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,16 +1,17 @@
+import { useEffect, useMemo } from 'react'
 import { UnifiThemeProvider, Themes } from '@unifiprotocol/uikit'
 import { Blockchains, TokenLogoResolvers } from '@unifiprotocol/utils'
 import { ShellWrappedComp } from '@unifiprotocol/shell'
 import { Body } from './Template/Body'
 import { Bridge } from './View'
 import { I18nextProvider } from 'react-i18next'
-import { useEffect, useMemo } from 'react'
 import { AdapterContext } from './Adapter'
 import { Config } from './Config/Component'
 import { Liquidty } from './Liquidity/Component'
 import { Swap } from './Swap/Component'
 import Clocks from './Services/Clocks'
 import { Transactions } from './Transactions/Component'
+import { RecoilRoot } from 'recoil'
 
 const App: ShellWrappedComp = ({ i18n, connection, balances, eventBus }) => {
   const blockchain = useMemo(
@@ -23,24 +24,26 @@ const App: ShellWrappedComp = ({ i18n, connection, balances, eventBus }) => {
   }, [])
 
   return (
-    <AdapterContext.Provider value={{ connection, balances, eventBus }}>
-      <I18nextProvider i18n={i18n}>
-        <UnifiThemeProvider
-          theme={Themes.Dark}
-          options={{
-            tokenLogoResolver: TokenLogoResolvers[blockchain]
-          }}
-        >
-          <Config />
-          <Liquidty />
-          <Swap />
-          <Transactions />
-          <Body>
-            <Bridge />
-          </Body>
-        </UnifiThemeProvider>
-      </I18nextProvider>
-    </AdapterContext.Provider>
+    <RecoilRoot>
+      <AdapterContext.Provider value={{ connection, balances, eventBus }}>
+        <I18nextProvider i18n={i18n}>
+          <UnifiThemeProvider
+            theme={Themes.Dark}
+            options={{
+              tokenLogoResolver: TokenLogoResolvers[blockchain]
+            }}
+          >
+            <Config />
+            <Liquidty />
+            <Swap />
+            <Transactions />
+            <Body>
+              <Bridge />
+            </Body>
+          </UnifiThemeProvider>
+        </I18nextProvider>
+      </AdapterContext.Provider>
+    </RecoilRoot>
   )
 }
 
