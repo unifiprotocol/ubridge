9022dbd22cec92885aac238cc90aa458a3b972f4
Daniel Blanco Parla <dblancoparla@gmail.com>
2022-08-30 16:06:01 +0200

    feat: webpack build

---
+++ app/webpack.config.prod.js
@@ -0,0 +1,69 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ProvidePlugin, container } = require('webpack');
+const { ModuleFederationPlugin } = container
+const { dependencies } = require('./package.json')
+const path = require('path');
+
+module.exports = {
+    entry: './src/index',
+    mode: 'production',
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        port: 3002,
+    },
+    output: {
+        publicPath: 'auto',
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+        plugins: [],
+        fallback: {
+            crypto: require.resolve('crypto-browserify'),
+            scrypt: require.resolve('scrypt-js'),
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                loader: 'file-loader',
+            },
+            {
+                test: /\.tsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: [['@babel/preset-react'], '@babel/preset-typescript'],
+                },
+            },
+        ],
+    },
+    plugins: [
+        new ProvidePlugin({
+            process: 'process/browser',
+            React: 'react'
+        }),
+        new ModuleFederationPlugin({
+            name: 'ubridge',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './Ubridge': './src/App',
+            },
+            shared: {
+                'react': {
+                    singleton: true,
+                    requiredVersion: dependencies['react'],
+                },
+                'react-dom': {
+                    singleton: true,
+                    requiredVersion: dependencies['react-dom'],
+                },
+            },
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        }),
+    ],
+};
