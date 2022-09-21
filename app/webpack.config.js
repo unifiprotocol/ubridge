863f3278877433252d7d6e5a7bff5e2dd7ec78d5
Carlos Lostao <hastro1990@gmail.com>
2022-09-21 19:23:12 +0200

    fix: buffer plugin missing in webpack config

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,77 +1,81 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { ProvidePlugin, container } = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { ProvidePlugin, container } = require('webpack')
 const { ModuleFederationPlugin } = container
 const { dependencies } = require('./package.json')
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-    entry: './src/index',
-    mode: 'development',
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist'),
-        },
-        port: 3002,
+  entry: './src/index',
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist')
     },
-    output: {
-        publicPath: 'auto',
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js'],
-        plugins: [],
-        fallback: {
-            crypto: require.resolve('crypto-browserify'),
-            scrypt: require.resolve('scrypt-js'),
+    port: 3002
+  },
+  output: {
+    publicPath: 'auto'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    plugins: [],
+    fallback: {
+      crypto: require.resolve('crypto-browserify'),
+      scrypt: require.resolve('scrypt-js')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [['@babel/preset-react'], '@babel/preset-typescript']
         }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                loader: 'file-loader',
-            },
-            {
-                test: /\.tsx?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                options: {
-                    presets: [['@babel/preset-react'], '@babel/preset-typescript'],
-                },
-            },
-        ],
-    },
-    plugins: [
-        new ProvidePlugin({
-            process: 'process/browser',
-            React: 'react'
-        }),
-        new ModuleFederationPlugin({
-            name: 'ubridge',
-            filename: 'remoteEntry.js',
-            exposes: {
-                './Ubridge': './src/App',
-            },
-            shared: {
-                'react': {
-                    singleton: true,
-                    requiredVersion: dependencies['react'],
-                },
-                'react-dom': {
-                    singleton: true,
-                    requiredVersion: dependencies['react-dom'],
-                },
-                '@unifiprotocol/shell': {
-                    singleton: true,
-                    requiredVersion: dependencies['@unifiprotocol/shell'],
-                },
-                '@unifiprotocol/uikit': {
-                    singleton: true,
-                    requiredVersion: dependencies['@unifiprotocol/uikit'],
-                },
-            },
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        }),
-    ],
-};
+      }
+    ]
+  },
+  plugins: [
+    new ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer']
+    }),
+    new ProvidePlugin({
+      process: 'process/browser',
+      React: 'react'
+    }),
+    new ModuleFederationPlugin({
+      name: 'ubridge',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Ubridge': './src/App'
+      },
+      shared: {
+        react: {
+          singleton: true,
+          requiredVersion: dependencies['react']
+        },
+        'react-dom': {
+          singleton: true,
+          requiredVersion: dependencies['react-dom']
+        },
+        '@unifiprotocol/shell': {
+          singleton: true,
+          requiredVersion: dependencies['@unifiprotocol/shell']
+        },
+        '@unifiprotocol/uikit': {
+          singleton: true,
+          requiredVersion: dependencies['@unifiprotocol/uikit']
+        }
+      }
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+}
