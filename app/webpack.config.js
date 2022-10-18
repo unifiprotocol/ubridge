7d0bba1c7ba9d6868897b841fc24a9c018248191
Daniel Blanco Parla <dblancoparla@gmail.com>
2022-10-18 16:15:24 +0200

    fix: webpack favicon

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -70,7 +70,8 @@ module.exports = {
       }
     }),
     new HtmlWebpackPlugin({
-      template: './public/index.html'
+      template: './public/index.html',
+      favicon: './public/favicon.ico'
     })
   ]
 }
